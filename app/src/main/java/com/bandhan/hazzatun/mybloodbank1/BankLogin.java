a423ef61724e1ab37bfcc4d0702202d0e553e27e
Hazzatun <hazzatun.jahan.cse@ulab.edu.bd>
2019-04-02 00:57:40 +0600

    Create login Pages each.

---
+++ app/src/main/java/com/bandhan/hazzatun/mybloodbank1/BankLogin.java
@@ -0,0 +1,13 @@
+package com.bandhan.hazzatun.mybloodbank1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BankLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_login);
+    }
+}
